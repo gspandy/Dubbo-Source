28c4bdb55d026a2a16edeef97abe8a7d093bca66
caianqi <anqicai@roseonly.com>
2019-12-26 13:43:46 +0800

    笔记

--- dubbo-master/dubbo-metadata/dubbo-metadata-api/src/main/java/org/apache/dubbo/metadata/report/identifier/KeyTypeEnum.java
+++
@@ -1,8 +0,0 @@
-package org.apache.dubbo.metadata.report.identifier;
-
-/**
- * 2019-08-15
- */
-public enum KeyTypeEnum {
-    PATH, UNIQUE_KEY
-}
--- dubbo-master/dubbo-plugin/dubbo-qos/src/test/java/org/apache/dubbo/qos/command/impl/TestInterface2.java
+++
@@ -1,9 +0,0 @@
-package org.apache.dubbo.qos.command.impl;
-
-/**
- * 2019-10-11
- */
-public interface TestInterface2 {
-
-    String sayHello();
-}
